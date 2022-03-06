fe57f282d60655fb1ea73cc865b28fb2db2ae7b1
FlameMyGame <dlgunnell@outlook.com>
2022-03-06 09:11:44 -0700

    added transitions, changed font link, added additional menu foler and jsx/scss files

--- portfolio/src/components/topbar/Topbar.jsx
+++ portfolio/src/components/topbar/Topbar.jsx
@@ -1,21 +1,30 @@
 import "./topbar.scss"
 import {ContactMail, ContactPhone} from '@material-ui/icons';
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className='topbar'>
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
-          <div className="left">
-              <a href="#intro" className="logo">genius.</a>
-              <div className="itemContainer">
-                <ContactMail/>
-                <span>(480)-532-9775</span>
-                <ContactPhone/>
-                <span>dlgunnell@outlook.com</span>
-              </div>
+        <div className="left">
+          <a href="#intro" className="logo">
+            genius.
+          </a>
+          <div className="itemContainer">
+            <ContactPhone className="icon"/>
+            <span>(480)-532-9775</span>
+          </div>  
+          <div className="itemContainer">
+            <ContactMail className="icon"/>
+            <span>dlgunnell@outlook.com</span>
           </div>
-          <div className="right">              
-          </div>
+        </div>
+        <div className="right">
+            <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+              <span className="line1"></span>
+              <span className="line2"></span>
+              <span className="line3"></span>
+            </div>
+        </div>
       </div>
     </div>
   )
